c85bff39a2a83c8524e6fe828573c6b250333f81
Eitan Konigsburg <eitan.konigsburg@gmail.com>
2011-06-16 16:10:28 -0700

    a bit of code cleanup b/c self code reviews are hard

--- src/rhino/cli.js
+++ src/rhino/cli.js
@@ -1,6 +1,6 @@
 importPackage(java.io);
 
-(function(argsArray) {
+(function (argsArray) {
     var files = [];
 
     if (argsArray.length === 0) {
@@ -8,27 +8,26 @@ importPackage(java.io);
         quit(1);
     }
 
-    var getFiles  = function(dir) {
+    var getFiles  = function (dir) {
         var files = [];
 
-        var traverse = function(dir, stack) {
+        var traverse = function (dir) {
             var dirList = dir.listFiles();
-            dirList.forEach(function(file) {
+            dirList.forEach(function (file) {
                 if (/\.css$/.test(file)) {
                     files.push(file);
-                }
-                else if (file.isDirectory()) {
-                    traverse(file, stack);
+                } else if (file.isDirectory()) {
+                    traverse(file);
                 }
             });
         };
 
-        traverse(dir, []);
+        traverse(dir);
 
         return files;
     };
 
-    argsArray.forEach(function(arg) {
+    argsArray.forEach(function (arg) {
         var curFile = new File(arg);
 
         if (!curFile.exists()) {
@@ -38,13 +37,12 @@ importPackage(java.io);
 
         if (curFile.isDirectory()) {
             files = files.concat(getFiles(curFile));
-        }
-        else {
+        } else {
             files.push(arg);
         }
     });
 
-    files.forEach(function(filename) {
+    files.forEach(function (filename) {
         var input = readFile(filename);
         if (!input) {
             print("csslint: Could not read file data in " + filename + ". Is the file empty?");
@@ -57,32 +55,28 @@ importPackage(java.io);
         if (messages.length > 0) {
             print("\n\n\ncsslint: There are " + messages.length + " errors and warnings in " + filename + ".");
 
-            messages.sort(function(a,b) {
+            messages.sort(function (a, b) {
                 if (a.rollup && !b.rollup) {
                     return 1;
-                }
-                else if (!a.rollup && b.rollup) {
+                } else if (!a.rollup && b.rollup) {
                     return -1;
-                }
-                else {
+                } else {
                     return 0;
                 }
             });
 
-            messages.forEach(function(message, i) {
+            messages.forEach(function (message, i) {
                 print("\n" + filename);
                 if (message.rollup) {
                     print("" + (i+1) + ": " + message.type);
                     print(message.message);
-                }
-                else {
+                } else {
                     print("" + (i+1) + ": " + message.type + " at line " + message.line + ", col " + message.col);
                     print(message.message);
                     print(message.evidence);
                 }
             });
-        }
-        else {
+        } else {
             print("csslint: No problems found in " + filename);
         }
     });
