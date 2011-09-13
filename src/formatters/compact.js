75fd53ef8f7d5c4d2aa7c36378eaab186be0d213
Eric Wendelin <emwendelin@gmail.com>
2011-09-12 21:08:53 -0600

    Switching to relative paths for compact format

--- src/formatters/compact.js
+++ src/formatters/compact.js
@@ -2,37 +2,76 @@ CSSLint.addFormatter({
     //format information
     id: "compact",
     name: "Compact, 'porcelain' format",
-    
-    startFormat: function(){
-        return "";
-    },
-    
-    endFormat: function(){
+
+    /**
+     * Return content to be printed before all file results.
+     * @return {String} to prepend before all results
+     */
+    startFormat: function() {
         return "";
     },
 
-    formatResults: function(results, filename) {
+    /**
+     * Return content to be printed after all file results.
+     * @return {String} to append after all results
+     */
+    endFormat: function() {
+        return "";
+    },
+
+    /**
+     * Given CSS Lint results for a file, return output for this format.
+     * @param results {Object} with error and warning messages
+     * @param filename {String} absolute file path
+     * @param options {Object} (Optional) specifies special handling of output
+     * @return {String} output for results
+     */
+    formatResults: function(results, filename, options) {
         var messages = results.messages,
             output = "",
-            pos = filename.lastIndexOf("/"),
-            shortFilename = filename;
+            relativeFilename = filename;
+
+        /**
+         * Given a source directory and a target filename, return the relative
+         * file path from source to target.
+         * @param source {String} directory path to start from for traversal
+         * @param target {String} directory path and filename to seek from source
+         * @return Relative path (e.g. "../../style.css") as {String}
+         */
+        function getRelativePath(source, target) {
+            var sep = (source.indexOf("/") !== -1) ? "/" : "\\",
+                targetArr = target.split(sep),
+                sourceArr = source.split(sep),
+                file = targetArr.pop(),
+                targetPath = targetArr.join(sep),
+                relativePath = "";
+        
+            while (targetPath.indexOf(sourceArr.join(sep)) === -1) {
+                sourceArr.pop();
+                relativePath += ".." + sep;
+            }
+        
+            var relPathArr = targetArr.slice(sourceArr.length);
+            relPathArr.length && (relativePath += relPathArr.join(sep) + sep);
+        
+            return relativePath + file;
+        }
 
         if (messages.length === 0) {
-            return shortFilename + ": Lint Free!";
+            return relativeFilename + ": Lint Free!";
         }
 
-        if (pos == -1){
-            pos = filename.lastIndexOf("\\");       
-        }
-        if (pos > -1){
-            shortFilename = filename.substring(pos+1);
+        options = options || {};
+
+        if (options.workingDirectory) {
+            relativeFilename = getRelativePath(options.workingDirectory, filename)
         }
 
-        messages.forEach(function (message, i) {
+        messages.forEach(function(message, i) {
             if (message.rollup) {
-                output += shortFilename + ": " + message.message + "\n";
+                output += relativeFilename + ": " + message.message + "\n";
             } else {
-                output += shortFilename + ": " + "line " + message.line + 
+                output += relativeFilename + ": " + "line " + message.line + 
                     ", col " + message.col + ", " + message.message + "\n";
             }
         });
--- tests/formatters/compact.js
+++ tests/formatters/compact.js
@@ -5,13 +5,13 @@
 
     YUITest.TestRunner.add(new YUITest.TestCase({
         name: "Compact formatter",
-        
-        "File with no problems should say so": function(){
+
+        "File with no problems should say so": function() {
             var result = { messages: [], stats: [] };
-            Assert.areEqual("FILE: Lint Free!", CSSLint.format(result, "FILE", "compact"));
+            Assert.areEqual("FILE: Lint Free!", CSSLint.getFormatter("compact").formatResults(result, "FILE"));
         },
 
-        "File with problems should list them": function(){
+        "File with problems should list them": function() {
             var result = { messages: [ 
                      { type: 'warning', line: 1, col: 1, message: 'BOGUS WARNING', evidence: 'BOGUS', rule: [] },
                      { type: 'error', line: 2, col: 1, message: 'BOGUS ERROR', evidence: 'BOGUS', rule: [] }
@@ -19,7 +19,31 @@
                 error1 = "FILE: line 1, col 1, BOGUS WARNING\n",
                 error2 = "FILE: line 2, col 1, BOGUS ERROR\n",
                 expected = error1 + error2,
-                actual = CSSLint.format(result, "FILE", "compact");
+                actual = CSSLint.getFormatter("compact").formatResults(result, "FILE");
+            Assert.areEqual(expected, actual);
+        },
+
+        "Should output relative file paths when given a workingDirectory": function() {
+            var result = { messages: [ 
+                     { type: 'warning', line: 1, col: 1, message: 'BOGUS WARNING', evidence: 'BOGUS', rule: [] },
+                     { type: 'error', line: 2, col: 1, message: 'BOGUS ERROR', evidence: 'BOGUS', rule: [] }
+                ], stats: [] },
+                error1 = "path/to/FILE: line 1, col 1, BOGUS WARNING\n",
+                error2 = "path/to/FILE: line 2, col 1, BOGUS ERROR\n",
+                expected = error1 + error2,
+                actual = CSSLint.getFormatter("compact").formatResults(result, "/absolute/path/to/FILE", {workingDirectory: "/absolute"});
+            Assert.areEqual(expected, actual);
+        },
+
+        "Should output absolute file paths by default": function() {
+            var result = { messages: [ 
+                     { type: 'warning', line: 1, col: 1, message: 'BOGUS WARNING', evidence: 'BOGUS', rule: [] },
+                     { type: 'error', line: 2, col: 1, message: 'BOGUS ERROR', evidence: 'BOGUS', rule: [] }
+                ], stats: [] },
+                error1 = "/absolute/path/to/FILE: line 1, col 1, BOGUS WARNING\n",
+                error2 = "/absolute/path/to/FILE: line 2, col 1, BOGUS ERROR\n",
+                expected = error1 + error2,
+                actual = CSSLint.getFormatter("compact").formatResults(result, "/absolute/path/to/FILE");
             Assert.areEqual(expected, actual);
         }
 
