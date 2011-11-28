61b87e9f0a4ca860e09f933de7e54d223a2a0a9d
Nicholas C. Zakas <nicholas.c.zakas@nczonline.net>
2011-11-28 14:27:13 -0800

    More tests for gradients rule

--- tests/rules/gradients.js
+++ tests/rules/gradients.js
@@ -49,7 +49,12 @@ background: -ms-linear-gradient(top, #1e5799 ,#2989d8 ,#207cca ,#7db9e8 );
             Assert.areEqual(1, result.messages.length);
             Assert.areEqual("warning", result.messages[0].type);
             Assert.areEqual("Missing vendor-prefixed CSS gradients for Firefox 3.6+, Webkit (Safari 5+, Chrome), Internet Explorer 10+, Opera 11.1+.", result.messages[0].message);
-        } 
+        },
+
+        "Using all vendor-prefixed gradients should not result in a warning": function(){
+            var result = CSSLint.verify("div.box {\n    background: -moz-linear-gradient(top,  #1e5799 0%, #7db9e8 100%);\n    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#1e5799), color-   stop(100%,#7db9e8));\n    background: -webkit-linear-gradient(top,  #1e5799 0%,#7db9e8 100%);\n    background: -o-linear-gradient(top,  #1e5799 0%,#7db9e8 100%);\n    background: -ms-linear-gradient(top,  #1e5799 0%,#7db9e8 100%); \n}", { "gradients": 1 });
+            Assert.areEqual(0, result.messages.length);
+        }
     }));
 
 })();
