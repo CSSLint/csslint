e38b87d206e597d655122fed2f0fa78e1d89518f
Nicholas C. Zakas <nicholas@nczconsulting.com>
2012-01-31 10:31:42 -0800

    Implemented rule to avoid unqualified attribute selectors (fixes 218)

---
+++ src/rules/unqualified-attributes.js
@@ -0,0 +1,42 @@
+/*
+ * Rule: Don't use unqualified attribute selectors because they're just like universal selectors.
+ */
+/*global CSSLint*/
+CSSLint.addRule({
+
+    //rule information
+    id: "unqualified-attributes",
+    name: "Disallow unqualified attribute selectors",
+    desc: "Unqualified attribute selectors are known to be slow.",
+    browsers: "All",
+
+    //initialization
+    init: function(parser, reporter){
+        var rule = this;
+
+        parser.addListener("startrule", function(event){
+            
+            var selectors = event.selectors,
+                selector,
+                part,
+                modifier,
+                i, j, k;
+
+            for (i=0; i < selectors.length; i++){
+                selector = selectors[i];
+                
+                part = selector.parts[selector.parts.length-1];
+                if (part.type == parser.SELECTOR_PART_TYPE){
+                    for (k=0; k < part.modifiers.length; k++){
+                        modifier = part.modifiers[k];
+                        if (modifier.type == "attribute" && (!modifier.elementName || modifier.elementName == "*")){
+                            reporter.report(rule.desc, modifier.line, modifier.col, rule);                               
+                        }
+                    }
+                }
+                
+            }            
+        });
+    }
+
+});
---
+++ tests/rules/unqualified-attributes.js
@@ -0,0 +1,31 @@
+(function(){
+
+    /*global YUITest, CSSLint*/
+    var Assert = YUITest.Assert;
+
+    YUITest.TestRunner.add(new YUITest.TestCase({
+    
+        name: "Unqualified Attributes Errors",
+
+        "Using an unqualified attribute selector alone should result in a warning": function(){
+            var result = CSSLint.verify("[type=text] { font-size: 10px; }", {"unqualified-attributes": 1 });
+            Assert.areEqual(1, result.messages.length);
+            Assert.areEqual("warning", result.messages[0].type);
+            Assert.areEqual("Unqualified attribute selectors are known to be slow.", result.messages[0].message);
+        },
+
+        "Using an unqualified attribute selector as the right-most part should result in a warning": function(){
+            var result = CSSLint.verify("p div [type=text] { font-size: 10px; }", {"unqualified-attributes": 1 });
+            Assert.areEqual(1, result.messages.length);
+            Assert.areEqual("warning", result.messages[0].type);
+            Assert.areEqual("Unqualified attribute selectors are known to be slow.", result.messages[0].message);
+        },
+
+        "Using an unqualified attribute selector in the middle should not result in a warning": function(){
+            var result = CSSLint.verify("[type=text] .foo { font-size: 10px; } ", {"unqualified-attributes": 1 });
+            Assert.areEqual(0, result.messages.length);
+        }
+        
+    }));
+
+})();
