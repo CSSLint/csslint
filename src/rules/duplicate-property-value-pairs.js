0485042806bc6d24a86b6562bd94d2f31cb31258
Brett Stimmerman <brettstimmerman@gmail.com>
2012-09-09 18:11:28 -0700

    Add duplicate property-value pairs rule

---
+++ src/rules/duplicate-property-value-pairs.js
@@ -0,0 +1,54 @@
+/*
+ * Rule: Be aware of duplicate property-value pairs.
+ */
+/*global CSSLint*/
+CSSLint.addRule({
+
+    //rule information
+    id: "duplicate-property-value-pairs",
+    name: "Duplicate property-value pairs",
+    desc: "Be aware of duplicate property-value pairs. Many duplicates may indicate the need for abstratcion.",
+    browsers: "All",
+
+    //initialization
+    init: function(parser, reporter){
+        var rule = this;
+        var count = {};
+
+        //count how many times "float" is used
+        parser.addListener("property", function(event){
+            var prop = event.property.text.toLowerCase(),
+                val  = event.value.text.toLowerCase(),
+                key  = prop + ':' + val;
+
+            if (!count[key]) {
+                count[key] = 0;
+            }
+
+            count[key] += 1;
+        });
+
+        //report the results
+        parser.addListener("endstylesheet", function(){
+            var data = [],
+                msg  = [];
+
+            for (var prop in count) {
+                if (count.hasOwnProperty(prop)) {
+                    data.push([prop, count[prop]]);
+                }
+            }
+
+            data.sort(function (a, b) {
+                return b[1] - a[1];
+            });
+
+            data = data.map(function (item) {
+                return item[1] + ' => ' + item[0];
+            }).join('\n');
+
+            reporter.rollupWarn('Duplicate property-value-pairs:\n\n' + data);
+        });
+    }
+
+});
---
+++ tests/rules/duplicate-property-value-pairs.js
@@ -0,0 +1,18 @@
+(function(){
+
+    /*global YUITest, CSSLint*/
+    var Assert = YUITest.Assert;
+
+    YUITest.TestRunner.add(new YUITest.TestCase({
+
+        name: "Duplicate Property-Value Pair Rule Errors",
+
+        "Duplicate property-value pairs should result in a warning": function () {
+            var result = CSSLint.verify(".foo { color: #f00; } .bar { color: #f00; }", {"duplicate-property-value-pairs": 1});
+            Assert.areEqual(1, result.messages.length);
+            Assert.areEqual("warning", result.messages[0].type);
+            Assert.areEqual("Duplicate property-value-pairs:\n\n2 => color:#f00", result.messages[0].message);
+        }
+    }));
+
+})();
