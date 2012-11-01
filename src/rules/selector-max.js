c03333af257eba1a2191371961c1801d9242c846
Philip Walton <philip@philipwalton.com>
2012-11-01 16:09:41 -0700

    Updated the selector-max and selector-max-approaching rules to apply to CSS selectors not just CSS rules.

--- src/rules/selector-max.js
+++ src/rules/selector-max.js
@@ -6,24 +6,23 @@ CSSLint.addRule({
 
     //rule information
     id: "selector-max",
-    name: "Error when past the 4095 limit for IE",
+    name: "Error when past the 4095 selector limit for IE",
     desc: "Will error when selector count is > 4095.",
     browsers: "IE",
 
     //initialization
     init: function(parser, reporter){
-		var rule = this,
-			count = 0;
+        var rule = this, count = 0;
 
-		parser.addListener('startrule',function(event){
-			count++;
-		});
+        parser.addListener('startrule',function(event) {
+            count += event.selectors.length;
+        });
 
-		parser.addListener("endstylesheet", function(){
-			if(count>4095){
-				reporter.report("You have "+count+" rules. Internet Explorer supports a maximum of 4095 rules. All additional rules will be ignored by IE. Consider refactoring.",0,0,rule);	
-			}
-		});
-	}
+        parser.addListener("endstylesheet", function() {
+            if (count > 4095) {
+                reporter.report("You have " + count + " selectors. Internet Explorer supports a maximum of 4095 selectors per stylesheet. Consider refactoring.",0,0,rule); 
+            }
+        });
+    }
 
 });
