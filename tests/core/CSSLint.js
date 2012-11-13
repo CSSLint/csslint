283303cdfb346580184f00c4e8baf03f8b3e84de
Nicholas C. Zakas <nicholas@nczconsulting.com>
2012-11-13 13:16:15 -0800

    Fix YUI Test so test failures stop build

--- tests/core/CSSLint.js
+++ tests/core/CSSLint.js
@@ -6,12 +6,12 @@
     YUITest.TestRunner.add(new YUITest.TestCase({
 
         name: "CSSLint object tests",
-        
+
         "Adjoining classes should not cause an error": function(){
             var result = CSSLint.verify(".foo.bar{}", { });
             Assert.areEqual(0, result.messages.length);
         },
-        
+
         "@media (max-width:400px) should not cause an error": function(){
             var result = CSSLint.verify("@media (max-width:400px) {}", { });
             Assert.areEqual(0, result.messages.length);
