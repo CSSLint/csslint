9b5c41bf6c32f0daf4ff5f2862a14623e7f1c08d
Nick Schonning <nschonni@gmail.com>
2013-04-07 13:50:30 -0400

    Add Full license block banner
    - Added date parsing so the current year is used in the copyright block
    - Added the version number to the build info block outside the copyright section

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,36 @@ module.exports = function(grunt) {
     grunt.initConfig({
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        banner: {
+            compact: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
                 '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
                 '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
                 '* Copyright (c) <%= grunt.template.today("yyyy") %> Nicole Sullivan and Nicholas C. Zakas;\n' +
                 '* Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> <%= _.pluck(pkg.licenses, "url").join(", ") %> */\n',
+            full: '/*!\n' +
+                'CSSLint\n' +
+                'Copyright (c) <%= grunt.template.today("yyyy") %> Nicole Sullivan and Nicholas C. Zakas. All rights reserved.\n' +
+                '\n' +
+                'Permission is hereby granted, free of charge, to any person obtaining a copy\n' +
+                'of this software and associated documentation files (the "Software"), to deal\n' +
+                'in the Software without restriction, including without limitation the rights\n' +
+                'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n' +
+                'copies of the Software, and to permit persons to whom the Software is\n' +
+                'furnished to do so, subject to the following conditions:\n' +
+                '\n' +
+                'The above copyright notice and this permission notice shall be included in\n' +
+                'all copies or substantial portions of the Software.\n' +
+                '\n' +
+                'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n' +
+                'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n' +
+                'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n' +
+                'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n' +
+                'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n' +
+                'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n' +
+                'THE SOFTWARE.\n\n' +
+                '*/\n' +
+                '/* Build: v<%= pkg.version %> <%= grunt.template.today("dd-mmmm-yyyy hh:MM:ss") %> */'
+        },
         build_dir: 'build',
         //Parser lib copy for verions that can't user requirejs
         parserlib: 'node_modules/parserlib/lib/node-parserlib.js',
@@ -36,7 +61,7 @@ module.exports = function(grunt) {
         concat: {
             core: {
                 options: {
-                    banner: '<%= banner %>\n' +
+                    banner: '<%= banner.full %>\n' +
                             //Hack for using the node version of parserlib
                             'var exports = exports || {};\n' +
                             'var CSSLint = (function(){\n',
@@ -57,7 +82,7 @@ module.exports = function(grunt) {
             },
             node: {
                 options: {
-                    banner: '<%= banner %>\n' +
+                    banner: '<%= banner.full %>\n' +
                             'var parserlib = require("parserlib");\n',
                     footer: '\nexports.CSSLint = CSSLint;'
                 },
@@ -68,7 +93,7 @@ module.exports = function(grunt) {
             },
             node_cli: {
                 options: {
-                    banner: '#!/usr/bin/env node\n<%= banner %>'
+                    banner: '#!/usr/bin/env node\n<%= banner.full %>'
                 },
                 src: [
                     'src/cli/common.js',
@@ -85,7 +110,7 @@ module.exports = function(grunt) {
             },
             worker: {
                 options: {
-                    banner: '<%= banner %>\n' +
+                    banner: '<%= banner.full %>\n' +
                             //Hack for using the node version of parserlib
                             'var exports = exports || {};\n'
                 },
