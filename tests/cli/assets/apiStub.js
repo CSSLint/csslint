e6c106789bcb6429efa4ea7dfc25093228dc229c
dmi3y <lapshukov@gmail.com>
2014-04-15 00:58:06 -0700

    wraping up with cli tests+
    CSSLint mix helper from utils to mix up cli and rc options

---
+++ tests/cli/assets/apiStub.js
@@ -0,0 +1,68 @@
+/*jshint node:true*/
+"use strict";
+var
+    stub = {
+        logbook: function (log) {
+            this.logs.push(log);
+        },
+        readLogs: function () {
+            return this.logs.slice();
+        },
+
+        getFullPath: function (path) {
+            return path;
+        },
+        getFiles: function (dir) {
+            var
+                filesobj = this.fakedFs[dir],
+                fileix,
+                out = [];
+            for (fileix in filesobj) {
+                if ( filesobj.hasOwnProperty(fileix) && /\.css$/.test(fileix) ) {
+                    out.push(dir + "/" + fileix);
+                }
+            }
+            return out;
+        },
+        readFile: function (path) {
+            var
+                spath = path.split("/"),
+                spathLen = spath.length,
+                i,
+                out = this.fakedFs;
+
+            for (i = 0; i < spathLen; i += 1) {
+                out = out[spath[i]];
+            }
+
+            return out;
+        },
+        isDirectory: function (checkit) {
+            var
+                result = this.fakedFs[checkit];
+            return typeof result === "object";
+        },
+        print: function (msg) {
+            this.logbook(msg);
+        },
+        quit: function (signal) {
+            this.logbook(signal);
+        }
+    };
+
+module.exports = function (setup) {
+    var
+        api,
+        setix;
+
+    api = Object.create(stub);
+
+    for (setix in setup) {
+        if (setup.hasOwnProperty(setix)) {
+            api[setix] = setup[setix];
+        }
+    }
+
+    api.logs = [];
+    return api;
+};
---
+++ tests/cli/assets/data.js
@@ -0,0 +1,67 @@
+/*jshint node:true*/
+module.exports = {
+    "suites": {
+        "config csslintrc override": {
+            "args": [
+                "--config=.rc1",
+                "dir"
+            ],
+            "expecting": [
+                "csslint: No errors in dir/a.css.",
+                "csslint: No errors in dir/b.css.",
+                0
+            ]
+        },
+        "straight linting": {
+            "args": [
+                "dir"
+            ],
+            "expecting": [
+                "csslint: There is 1 problem in dir/a.css.",
+                "csslint: There is 1 problem in dir/b.css.",
+                0
+            ]
+        },
+        "mix of cli options": {
+            "args": [
+                "--config=.rc1",
+                "--ignore=important",
+                "dir"
+            ],
+            "expecting": [
+                "csslint: No errors in dir/a.css.",
+                "csslint: There is 1 problem in dir/b.css.",
+                0
+            ]
+        },
+        "more mixes of cli options": {
+            "args": [
+                "--config=.rc1",
+                "--errors=important",
+                "dir"
+            ],
+            "expecting": [
+                "csslint: There is 1 problem in dir/a.css.",
+                "csslint: No errors in dir/b.css.",
+                1
+            ]
+        },
+        "version": {
+            "args": [
+                "--version"
+            ],
+            "expecting": [
+                "v@VERSION@",
+                0
+            ]
+        }
+    },
+
+    "fakedFs": {
+        ".rc1": "--ignore=important,ids",
+        "dir": {
+            "a.css": ".a {color: red!important;}",
+            "b.css": "#a {color: red;}"
+        },
+    }
+};
